ee6258f73d192d7d2942af6aae79e6d8ff0f782d
markus.gnigler <markus.gnigler@masp-software.eu>
2022-05-14 00:53:13 +0200

    Finished build script

--- buildx-and-push.ps1
+++ buildx-and-push.ps1
@@ -1,6 +1,7 @@
+# docker push ghcr.io/MarkusGnigler/n4d:latest
 docker buildx build `
     --platform linux/amd64,linux/arm64,linux/arm/v7 `
-    --tag registry.bit-shifter.at/ngind:latest `
+    --tag ghcr.io/markusgnigler/n4d:latest `
     --push `
     .
 
