e78602b26177a556dd4fbe473dbc267e11e4f9ba
markus.gnigler <markus.gnigler@masp-software.eu>
2022-05-17 13:15:34 +0200

    Remove buildx script for local push

--- buildx-and-push.ps1
+++
@@ -1,8 +0,0 @@
-# docker push ghcr.io/MarkusGnigler/n4d:latest
-docker buildx build `
-    --platform linux/amd64,linux/arm64,linux/arm/v7 `
-    --tag ghcr.io/markusgnigler/n4d:latest `
-    --push `
-    .
-
-exit 0
