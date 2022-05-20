a632036348d211f05b166a605d9a81e7c418397f
markus.gnigler <markus.gnigler@masp-software.eu>
2022-05-20 09:16:28 +0200

    Update path from envsubst find command

--- envsubst.sh
+++ envsubst.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find ./vhost.d -name '*.conf' | xargs -i sh -c "envsubst '\$HOST_IP \${HOST_IP}' < {} > {}.temp && mv {}.temp {}"
+find /etc/nginx/vhost.d -name '*.conf' | xargs -i sh -c "envsubst '\$HOST_IP \${HOST_IP}' < {} > {}.temp && mv {}.temp {}"
 
 exec "$@"
