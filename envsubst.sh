05aed0a95c730cfce36e774d8588b90eac5d1d2b
markus.gnigler <markus.gnigler@masp-software.eu>
2022-05-20 18:01:20 +0200

    Fix envsubst substitution of many variable

--- envsubst.sh
+++ envsubst.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 find /etc/nginx/vhost.d -name '*.conf' \
-| rev | awk -v FS='/' '{print $1}' | rev \
+| rev | awk -v FS='/' '{ print $1 }' | rev \
 | xargs -i sh -c \
-"envsubst \"$(cat $(pwd)/ENV | sed -e 's|^|\\|')\" < /etc/nginx/vhost.d/{} > /etc/nginx/internal-vhost.d/{}"
+"envsubst \"$( cat /ENV | sed -e 's|^|\\|' | tr '\n' ' ' )\" < /etc/nginx/vhost.d/{} > /etc/nginx/internal-vhost.d/{}"
 
 exec "$@"
